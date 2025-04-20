66de9ceb0c451b787b188039ab6a31041c1ab9ab
apachi1444 <yessinejawa@gmail.com>
2025-04-20 23:46:20 +0100

    feat(i18n): add support for Arabic, Russian, and Spanish languages
    This commit introduces new language resources (arJSON, ruJSON, esJSON) to the i18n configuration, enabling the application to support Arabic, Russian, and Spanish languages. This enhancement broadens the application's acce

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -6,12 +6,18 @@ import { initReactI18next } from "react-i18next";
 import enJSON from './en/en.json'
 import ptJSON from './pt/pt.json'
 import frJSON from './fr/fr.json'
+import arJSON from './ar/ar.json'
+import ruJSON from './ru/ru.json'
+import esJSON from './es/es.json'
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { ...enJSON },
     pt: { ...ptJSON },
     fr: { ...frJSON },
+    ar: {...arJSON },
+    ru: {...ruJSON },
+    es: {...esJSON }
   },
   lng: "en",
   fallbackLng: "en",
