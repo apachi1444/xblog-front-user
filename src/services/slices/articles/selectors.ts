c8af33dd807f7c61b0f7df2928af47f84fdc32f6
apachi1444 <yessinejawa@gmail.com>
2025-04-30 11:09:39 +0100

    refactor: remove unused article selectors to streamline state access

--- src/services/slices/articles/selectors.ts
+++ src/services/slices/articles/selectors.ts
@@ -1,11 +1,6 @@
 import type { RootState } from 'src/services/store';
 
 export const selectAllArticles = (state: RootState) => state.articles.articles;
-export const selectDraftArticles = (state: RootState) => state.articles.draftArticles;
-export const selectPublishedArticles = (state: RootState) => state.articles.publishedArticles;
 export const selectArticleCount = (state: RootState) => state.articles.count;
-export const selectArticlesByStore = (state: RootState, storeId: string) => 
-  state.articles.storeArticles[storeId] || [];
-export const selectCurrentArticle = (state: RootState) => state.articles.currentArticle;
 export const selectArticlesLoading = (state: RootState) => state.articles.loading;
 export const selectArticlesError = (state: RootState) => state.articles.error;
